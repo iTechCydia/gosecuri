ce3f30cdc904b48ce2b17fa461de1b66d33f739b
PaulineSolheim <paulinebouffioux@gmail.com>
2019-02-13 09:58:50 +0100

    Revert "lol"
    This reverts commit 4565cd6c4c50542d2614dc87f3728d20bc6d3534, reversing
    changes made to eb7d765e57d3e7cacdc9bbda38f372a74ff8df2d.

--- api-java/src/api/java/ApiJava.java
+++ api-java/src/api/java/ApiJava.java
@@ -15,6 +15,7 @@ public class ApiJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //Database db = new Database();
+        // TODO code application logic here
     }
+    
 }
--- ressources/README.md
+++
@@ -1,3 +0,0 @@
-#Ressources
-Le fichier `sqlite-jdbc.3.23.1.jar` est requis pour faire fonctionner SQLite avec NetBeans ou autre éditeur de code.
-Il n'est pas inclu dans `/api-java`, c'est dans les sources de l'éditeur.
